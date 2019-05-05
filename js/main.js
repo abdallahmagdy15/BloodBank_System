836d80291c7eb3f5692a068d7af10fa9407de0a2
abdallahmagdy15 <abdallah.magdy1515@gmail.com>
2019-05-05 07:16:46 +0200

    links works

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-﻿
+﻿$(function() {
+  var href = window.location.href;
+    console.log(1);
+  $('li.nav-item').each(function(e,i) {
+          console.log(2);
+    if (href.indexOf($(this).find("a").attr('href')) >= 0) {
+      $(this).addClass('active');
+        console.log(this);
+    }
+  });
+});
